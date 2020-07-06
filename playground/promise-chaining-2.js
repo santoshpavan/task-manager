9454852db0c00b70504b0b8b3b6bc50da41e1521
santoshpavan <santoshpavan.psp@gmail.com>
2020-07-06 17:26:50 -0400

    using async instead of promise-chaining in playground

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -2,11 +2,23 @@ require('../src/db/mongoose');
 const Task = require('../src/models/task');
 const { ResumeToken } = require('mongodb');
 
-Task.findByIdAndDelete('5f02499054945e04eb2277ed').then((task) => {
-    console.log(task);
-    return Task.countDocuments({completed: false});
-}).then((result) => {
-    console.log(result);
+// Task.findByIdAndDelete('5f02499054945e04eb2277ed').then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({completed: false});
+// }).then((result) => {
+//     console.log(result);
+// }).catch((e) => {
+//     console.log(e);
+// })
+
+const removeTask = async (id) => {
+    await Task.findByIdAndDelete(id);
+    const count = Task.countDocuments({completed: false});
+    return count;
+}
+
+removeTask('5f026520ab2186093e29d257').then((count) => {
+    console.log(count);
 }).catch((e) => {
     console.log(e);
-})
+});
