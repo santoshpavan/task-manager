f4290e2a3c41b42bc5aeaee47c794ca4ffbd285f
santoshpavan <santoshpavan.psp@gmail.com>
2020-07-07 10:51:36 -0400

    refactor: promise chaining to async await for tasks

--- src/index.js
+++ src/index.js
@@ -65,34 +65,57 @@ app.get('/users/:id', async(req, res) => {
     // });
 });
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async(req, res) => {
     const task = new Task(req.body);
 
-    task.save().then(() => {
+    try{
+        await task.save();
         res.status(201).send(task);
-    }).catch(() => {
-        res.status(400).send(error);
-    })
-})
-
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
-        res.send(tasks);
-    }).catch(() => {
-        res.status(500).send();
-    })
+    } catch(e) {
+        res.status(400).send();
+    }
+    // task.save().then(() => {
+    //     res.status(201).send(task);
+    // }).catch(() => {
+    //     res.status(400).send(error);
+    // });
 });
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks', async(req, res) => {
+    try{
+        const tasks = await Task.find({});
+        res.send(tasks);
+    } catch(e) {
+        res.status(500).send();
+    }
+    
+    // Task.find({}).then((tasks) => {
+    //     res.send(tasks);
+    // }).catch(() => {
+    //     res.status(500).send();
+    // });
+});
+
+app.get('/tasks/:id', async(req, res) => {
     const taskId = req.params.id;
-    Task.findById(taskId).then((task) => {
+    try {
+        const task = await Task.findById(taskId);
         if(!task) {
             return res.status(404).send();
         }
         res.send(task);
-    }).catch(() => {
-        res.status(500).send();
-    })
+    } catch(e) {
+        res.send(500).send();
+    }
+
+    // Task.findById(taskId).then((task) => {
+    //     if(!task) {
+    //         return res.status(404).send();
+    //     }
+    //     res.send(task);
+    // }).catch(() => {
+    //     res.status(500).send();
+    // });
 });
 
 app.listen(port, () => {
