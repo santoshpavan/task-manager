44c54bec529035d8cfe845b5a203fb9fc4e6e40a
santoshpavan <santoshpavan.psp@gmail.com>
2020-07-09 09:56:45 -0400

    Maintanence Mode for website using Middleware function

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ require('./db/mongoose'); //mangoose connects to the db
 const app = express();
 const port = process.env.PORT || 3000;
 
+// middleware function to reject all requests - Maintanence Mode
+app.use((req, res, next) => {
+    res.status(503).send("Website is currently under Maintanence. Please try again later.");
+});
+
 // this allows automatic parsing of JSON data being sent
 app.use(express.json());
 // registering the routers
