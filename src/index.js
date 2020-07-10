1ffc2f24248eb9264c086678d474ca3dabfdc453
santoshpavan <santoshpavan.psp@gmail.com>
2020-07-10 08:16:37 -0400

    experimenting on populating users and tasks relations

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 // the starting point of the application
 const express = require('express');
-const User = require('./models/user');
-const Task = require('./models/task');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
+const { translateAliases } = require('./models/task');
 require('./db/mongoose'); //mangoose connects to the db
 
 const app = express();
@@ -23,3 +22,21 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log('Server is on port: ' + port);
 });
+
+
+// experimental stuff
+const User = require('./models/user');
+const Task = require('./models/task');
+
+const main = async () => {
+    // const task = await Task.findById('5f085693f85a0f03ad5704c4');
+    // console.log(task.owner);
+    // //to populate the reference User profile
+    // await task.populate('owner').execPopulate();
+    // console.log(task.owner);
+    // const user = await User.findById('5f085639f85a0f03ad5704bc');
+    // await user.populate('tasks').execPopulate();
+    // console.log(user.tasks);
+}
+
+main();
