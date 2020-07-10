3b5f40e04bf0bcd8474d3726158fa73d40b8c68a
santoshpavan <santoshpavan.psp@gmail.com>
2020-07-10 11:47:01 -0400

    refactoring the cascade deletion

--- src/routers/task.js
+++ src/routers/task.js
@@ -70,12 +70,9 @@ router.patch('/tasks/:id', auth, async(req, res) => {
 router.delete('/tasks/:id', auth, async(req, res) => {
     const taskId = req.params.id;
     try{
-       const task = await Task.findOneAndDelete( {_id: taskId, owner: req.user._id} );
-       if(!task) {
-           return res.status(404).send();
-       }
-
-       res.send(task);
+        // populating the tasks part of the users with its tasks
+        await req.user.populate('tasks').execPopulate();
+        res.send(task);
     } catch(e) {
         res.send(500).send();
     }
