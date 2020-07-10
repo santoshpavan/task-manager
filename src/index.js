4395ac0d970e23c425f0b6ff4610f754184388a8
santoshpavan <santoshpavan.psp@gmail.com>
2020-07-10 08:23:28 -0400

    removed dead code

--- src/index.js
+++ src/index.js
@@ -8,11 +8,6 @@ require('./db/mongoose'); //mangoose connects to the db
 const app = express();
 const port = process.env.PORT || 3000;
 
-// middleware function to reject all requests - Maintanence Mode
-// app.use((req, res, next) => {
-//     res.status(503).send("Website is currently under Maintanence. Please try again later.");
-// });
-
 // this allows automatic parsing of JSON data being sent
 app.use(express.json());
 // registering the routers
@@ -22,21 +17,3 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log('Server is on port: ' + port);
 });
-
-
-// experimental stuff
-const User = require('./models/user');
-const Task = require('./models/task');
-
-const main = async () => {
-    // const task = await Task.findById('5f085693f85a0f03ad5704c4');
-    // console.log(task.owner);
-    // //to populate the reference User profile
-    // await task.populate('owner').execPopulate();
-    // console.log(task.owner);
-    // const user = await User.findById('5f085639f85a0f03ad5704bc');
-    // await user.populate('tasks').execPopulate();
-    // console.log(user.tasks);
-}
-
-main();
